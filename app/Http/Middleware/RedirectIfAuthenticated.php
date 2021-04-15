1a5e2595e8086828b218add5a87430cffec48a04
LEO <alvarezleom@gmail.com>
2021-04-15 00:38:11 -0300

    configuration for authenticate SPA

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,6 +19,8 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
+            if($request->wantsJson())
+                return response()->json($request->user());
             return redirect(RouteServiceProvider::HOME);
         }
 
